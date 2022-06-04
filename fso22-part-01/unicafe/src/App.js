a5461e2f6b4661c56121282676cc4e2c527f74d0
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-06-05 00:48:43 +0300

    add part 1 exercise 1.6: unicafe, step1

---
+++ fso22-part-01/unicafe/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Display = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    setGood(newValue)
+  }
+
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <h1> UNICAFE </h1>
+      <h3> Give feedback: </h3>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <h3> Statistics: </h3>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+    </div>
+  )
+}
+
+export default App
