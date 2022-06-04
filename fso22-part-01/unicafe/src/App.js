d8ab62ed9465beedb8f5b442c60db0a99f1779b2
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-06-05 01:51:43 +0300

    add part 1 exercise 1.7: unicafe, step2

--- fso22-part-01/unicafe/src/App.js
+++ fso22-part-01/unicafe/src/App.js
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
+// component for showing feedback statistics
 const Display = (props) => (
   <div>
-    {props.text} {props.value}
+    {props.text} {props.value} {props.text2}
   </div>
 )
 
+// component for button functions
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
@@ -17,6 +19,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const total = good + neutral + bad
+  const average = (((good * 1) + (neutral * 0) + (bad * (-1))) / total).toFixed(1)
+  const positive = ((good / total) * 100).toFixed(1)
 
   const setToGood = newValue => {
     setGood(newValue)
@@ -34,13 +40,25 @@ const App = () => {
     <div>
       <h1> UNICAFE </h1>
       <h3> Give feedback: </h3>
-      <Button handleClick={() => setToGood(good + 1)} text="good" />
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      
+      <Button handleClick={() => 
+        setToGood(good + 1)} text="good" />
+      
+      <Button handleClick={() => 
+        setToNeutral(neutral + 1)} text="neutral" />
+      
+      <Button handleClick={() => 
+        setToBad(bad + 1)} text="bad" />
+     
       <h3> Statistics: </h3>
+      
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value={total} />
+      <Display text="average" value={average}/>
+      <Display text="positive" value={positive} text2="%" />
+
     </div>
   )
 }
