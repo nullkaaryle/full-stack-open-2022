a91b5673044fbd9e9097886379728c0965281b2e
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2022-06-09 12:33:44 +0300

    add part 2 exercise 2.7: the phonebook, step 2

--- fso22-part-02/phonebook/src/App.js
+++ fso22-part-02/phonebook/src/App.js
@@ -8,24 +8,57 @@ const App = () => {
   // list of Person objects are saved in state
   const [persons, setPersons] = useState([])
 
+
   // text that the user types in name input field
   // is saved in state newName
   const [newName, setNewName] = useState('')
 
-  // takes care of adding a new person to persons list
-  // the last line empties the input element in form,
+
+  // takes care of checking if there already
+  // is a person with same name
+  // and if not, then adding the person
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (match) {
+      alertUser()
+    } else {
+      savePerson()
+    }
+  }
+
+
+  // check if the person with the same id 
+  // is already added to phonebook
+  // the id of the person uses lower case version of name
+  // so the match checking is done on lower case newName
+  const match = persons.find((person) => person.id === newName.toLowerCase())
+
+
+  // alert user with a pop up in browser
+  // that the person is already added to phonebook
+  const alertUser = () => {
+    window.alert(`${newName} is already added to phonebook`)
+  }
+
+
+  // creates a new Person and adds it to the persons list
+  // the id is also the name, but always in lower case
+  // this prevents adding "Jane doe" to phonebook
+  // if "Jane Doe" (or any other lower case/upper case combination) 
+  // is already added
+  // the second last line empties the input element in form,
   // if left out, the newName state would not change and
   // the added name would stay on the input field
   // after submitting (and user would have to erase it)
-  const addPerson = (event) => {
-    event.preventDefault()
+  const savePerson = () => {
     const personObject = {
       name: newName,
-      id: newName
+      id: newName.toLowerCase()
     }
     setPersons(persons.concat(personObject))
     setNewName('')
   }
+
 
   // follows the input field and
   // what the user is typing,
@@ -33,6 +66,7 @@ const App = () => {
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
   }
+
 
   return (
     <div>
